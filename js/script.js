8155db74e99edebd41547df825d97a3f3a7a2417
LarisaLG <lgalisheva@gmail.com>
2022-06-05 10:50:10 +0000

    Rename images for correct work of the script.js with an array of cards

--- js/script.js
+++ js/script.js
@@ -18,8 +18,8 @@ window.onload = function () {
 
 // Looping through values and suits and creates array of 52 cards
 function createCards() {
-    let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"];
-    let suits = ["C", "D", "H", "S"];
+    let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+    let suits = ["clubs", "diamonds", "hearts", "spades"];
     deck = [];
 
     //looping through card suits and values to create an array of 52 cards
